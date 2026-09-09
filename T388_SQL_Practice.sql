b713a8fda4ede9b37394d03dd0d8e18297043b90
“Sujal <ingalesujal05@gmail.com>
2026-09-09 17:05:48 +0530

    sql Basics update

---
+++ T388_SQL_Practice.sql
@@ -0,0 +1,67 @@
+use t388;
+select database();
+CREATE TABLE Employee (
+EmployeeId INT PRIMARY KEY,
+FullName VARCHAR(45) NOT NULL,
+Department VARCHAR(45) NOT NULL,
+Salary float NOT NULL,
+Gender VARCHAR(45) NOT NULL,
+Age INT NOT NULL
+);
+desc Employee;
+select * from employee;
+
+insert into employee values
+(1001,"Sujal","IT",35000,"Male",22);
+delete from employee;
+truncate table employee;
+
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25), 
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27), 
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28), 
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26), 
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25), 
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27), 
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28), 
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28), 
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+
+-- 07-09-2026
+use t388;
+show tables;
+select * from employee;
+delete from employee where gender ="Male";
+delete from employee where age>25;
+
+insert into employee values
+(1001,"John Doe","IT",35000,"Male",25), 
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27), 
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28), 
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26), 
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25), 
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27), 
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28), 
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28), 
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+
+
+
+
+-- 09-09-2026
+create table Kisan_Info
+(ID int unique not null,
+name varchar(50) unique not null,
+age int check (age>=18),
+email_id varchar (45) default "dummy@gmail.com"
+);
+desc Kisan_Info;
+show tables;
+insert into Kisan_Info
+values
+(10,"sumit",21,default);
+select * from Kisan_Info;
+alter table Kisan_Info modify age int check (age>20);
+
